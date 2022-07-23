fd3236b88ffd431b0a1e889fc934240a324d529c
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-07-23 15:54:13 +0300

    added userName to Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,17 @@
 // import { Outlet } from "react-router-dom";
 import style from '../СommonStyle/CommoneStyle.module.css'
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
+
 
 export default function Home() {
+    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+    const userName = useSelector(authSelectors.getUserEmail);
+
     return (
        <>
-        <h1 className={style.title}>Welcome to our website!</h1>
-        {/* <Outlet/> */}
+            {isLoggedIn?(<h1 className={style.title}>{userName}, welcome to our website!</h1>):(<h1 className={style.title}>Welcome to our website!</h1>)}
+ 
         </>
     )
 }
